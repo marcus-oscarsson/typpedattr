980cadd0cbd2664709d4ee20e9e113909dd79caf
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2015-05-22 16:50:51 +0200

    Added some more descriptions to the examples

--- examples/views.py
+++ examples/views.py
@@ -4,7 +4,10 @@ import _phome
 from typedattr.type import Int, Float, Str
 from typedattr.view.qt5 import Application, FormView
 
-# Create a model using 'typed attributes'
+# Create a person object with typed attributes, name, size, and age
+# Float and Int are given (defualt value, min value and max value)
+# Trying to set a value oustide of the permitted range sends the event
+# value_change_error. Another possibility would be to raise an Exception
 class Person(object):
     name = Str('A name', meta_options = {'label': 'Name', 'tooltip':'Name'})
     age = Int(43, 0, 150, meta_options = {'label': 'Age', 'tooltip':'Age'})
